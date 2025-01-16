19aaef0eab2859ba931eec23b03e2c2f959d3546
Ivan Teaca <ivan.teaca@gmail.com>
2025-01-16 22:59:31 +0200

    fix: remove duplicate migrations to resolve naming conflicts

--- db/migrate/20240116_add_indexes_to_applications.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexesToApplications < ActiveRecord::Migration[7.0]
-  def change
-    add_index :applications, :user_id, if_not_exists: true
-    add_index :applications, :status, if_not_exists: true
-    add_index :applications, :created_at, if_not_exists: true
-  end
-end 
--- db/migrate/XXX_add_indexes_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexesToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_index :users, :telegram_id, unique: true
-    add_index :users, :language
-  end
-end 
