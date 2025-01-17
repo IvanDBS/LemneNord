2a1322b02698b4a4873dc4c0b1d99033d9dbcc21
Ivan Teaca <ivan.teaca@gmail.com>
2025-01-17 14:32:00 +0200

    feat: add PostgreSQL support and Railway deployment configuration

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'telegram-bot-ruby'
-gem 'sqlite3'
+gem 'sqlite3', group: :development
+gem 'pg', group: :production
 gem 'activerecord'
 gem 'activesupport'
 gem 'rake'
--- config.rb
+++ config.rb
@@ -15,13 +15,24 @@ module Config
   CHANNEL_ID = ENV['TELEGRAM_CHANNEL_ID'].to_i
 
   # Конфигурация базы данных
-  DATABASE_CONFIG = {
-    adapter: ENV['DB_ADAPTER'],
-    database: ENV['DB_NAME'],
-    pool: ENV['DB_POOL'].to_i,
-    timeout: ENV['DB_TIMEOUT'].to_i,
-    checkout_timeout: 3
-  }.freeze
+  DATABASE_CONFIG = if ENV['RAILWAY_ENVIRONMENT'] == 'production'
+    {
+      adapter: 'postgresql',
+      encoding: 'unicode',
+      url: ENV['DATABASE_URL'],
+      pool: ENV.fetch('DB_POOL', 5).to_i,
+      timeout: ENV.fetch('DB_TIMEOUT', 3000).to_i,
+      checkout_timeout: 3
+    }.freeze
+  else
+    {
+      adapter: ENV['DB_ADAPTER'],
+      database: ENV['DB_NAME'],
+      pool: ENV['DB_POOL'].to_i,
+      timeout: ENV['DB_TIMEOUT'].to_i,
+      checkout_timeout: 3
+    }.freeze
+  end
 
   # Настройки планировщика
   SCHEDULER_CONFIG = {
